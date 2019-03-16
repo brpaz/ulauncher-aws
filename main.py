f7a076db16f3749661797c4905cc390493e0544c
Rufus Shrestha <rufus.shrestha@skyfii.com>
2019-03-16 17:35:53 +1100

    Fix for alternative options not showing, reformat code

--- main.py
+++ main.py
@@ -13,7 +13,15 @@ class GnomeSessionExtension(Extension):
 class KeywordQueryEventListener(EventListener):
     def on_event(self, event, extension):
         items = []
-        options = ['ec2', 'ecs', 'rds', 's3', 'elasticbeanstalk', 'elasticache', 'cloudwatch', 'cloudformation', 'route53', 'vpc', 'iam', 'ecr', 'eks', 'lambda', 'dynamodb', 'managementconsole', 'support', 'billing', 'pricingcalculator', 'compare']
+        options = [
+                        'ec2', 'ecs', 'rds', 's3', 'elasticbeanstalk', 'elasticache', 'cloudwatch', 'cloudformation', 'vpc', 'iam', 'ecr', 'eks', 'lambda', 'dynamodb',
+                        'managementconsole', 'management', 'console',
+                        'support', 'ticket', 'helpdesk', 'help',
+                        'billing', 'budget', 'costs',
+                        'pricingcalculator', 'pricing', 'price', 'prices', 'calculate', 'calculator',
+                        'compare', 'instancecomparison', 'comparison'
+                        'route53','dns'
+                  ]
         my_list = event.query.split(" ")
         if len(my_list) == 1:
             items.append(get_ec2_item())
@@ -24,7 +32,6 @@ class KeywordQueryEventListener(EventListener):
             items.append(get_elasticache_item())
             items.append(get_cloudwatch_item())
             items.append(get_cloudformation_item())
-            items.append(get_route53_item())
             items.append(get_vpc_item())
             items.append(get_iam_item())
             items.append(get_ecr_item())
@@ -36,28 +43,14 @@ class KeywordQueryEventListener(EventListener):
             items.append(get_billing_item())
             items.append(get_pricingcalculator())
             items.append(get_compare())
+            items.append(get_route53_item())
             return RenderResultListAction(items)
         else:
             my_query = my_list[1]
             included = []
             for option in options:
                 if my_query in option:
-                    if option in ['managementconsole', 'management', 'console'] and 'managementconsole' not in included:
-                        items.append(get_managementconsole_item())
-                        included.append('managementconsole')
-                    elif option in ['support', 'ticket', 'helpdesk', 'help'] and 'support' not in included:
-                        items.append(get_support_item())
-                        included.append('support')
-                    elif option in ['billing', 'budget', 'costs'] and 'billing' not in included:
-                        items.append(get_billing_item())
-                        included.append('billing')
-                    elif option in ['pricingcalculator', 'pricing', 'price', 'prices', 'calculate', 'calculator'] and 'pricingcalculator' not in included:
-                        items.append(get_pricingcalculator())
-                        included.append('pricingcalculator')
-                    elif option in ['compare', 'instancecomparison', 'comparison'] and 'compare' not in included:
-                        items.append(get_compare())
-                        included.append('compare')
-                    elif option in ['ec2']:
+                    if option in ['ec2']:
                         items.append(get_ec2_item())
                     elif option in ['ecs']:
                         items.append(get_ecs_item())
@@ -73,8 +66,6 @@ class KeywordQueryEventListener(EventListener):
                         items.append(get_cloudwatch_item())
                     elif option in ['cloudformation']:
                         items.append(get_cloudformation_item())
-                    elif option in ['route53']:
-                        items.append(get_route53_item())
                     elif option in ['vpc']:
                         items.append(get_vpc_item())
                     elif option in ['iam']:
@@ -87,6 +78,25 @@ class KeywordQueryEventListener(EventListener):
                         items.append(get_lambda_item())
                     elif option in ['dynamodb']:
                         items.append(get_dynamodb_item())
+                    elif option in ['managementconsole', 'management', 'console'] and 'managementconsole' not in included:
+                        items.append(get_managementconsole_item())
+                        included.append('managementconsole')
+                    elif option in ['support', 'ticket', 'helpdesk', 'help'] and 'support' not in included:
+                        items.append(get_support_item())
+                        included.append('support')
+                    elif option in ['billing', 'budget', 'costs'] and 'billing' not in included:
+                        items.append(get_billing_item())
+                        included.append('billing')
+                    elif option in ['pricingcalculator', 'pricing', 'price', 'prices', 'calculate', 'calculator'] and 'pricingcalculator' not in included:
+                        items.append(get_pricingcalculator())
+                        included.append('pricingcalculator')
+                    elif option in ['compare', 'instancecomparison', 'comparison'] and 'compare' not in included:
+                        items.append(get_compare())
+                        included.append('compare')
+                    elif option in ['route53', 'dns'] and 'route53' not in included:
+                        items.append(get_route53_item())
+                        included.append('route53')
+                        
             return RenderResultListAction(items)
 
 def get_ec2_item():
@@ -135,12 +145,6 @@ def get_cloudformation_item():
                                name='AWS CloudFormation',
                                description='AWS Cloud Formation Cosole',
                                on_enter=OpenUrlAction("https://console.aws.amazon.com/cloudformation"))
-
-def get_route53_item():
-    return ExtensionResultItem(icon='images/icon.png',
-                               name='AWS Route 53',
-                               description='AWS Route 53 Domain & DNS Service',
-                               on_enter=OpenUrlAction("https://console.aws.amazon.com/route53"))
 
 def get_vpc_item():
     return ExtensionResultItem(icon='images/icon.png',
@@ -207,6 +211,12 @@ def get_compare():
                                name='AWS Instance Comparision',
                                description='EC2Instances.info Easy Amazon EC2 Instance Comparison',
                                on_enter=OpenUrlAction("https://www.ec2instances.info"))
+
+def get_route53_item():
+    return ExtensionResultItem(icon='images/icon.png',
+                               name='AWS Route 53',
+                               description='AWS Route 53 Domain & DNS Service',
+                               on_enter=OpenUrlAction("https://console.aws.amazon.com/route53"))
 
 if __name__ == '__main__':
     GnomeSessionExtension().run()
